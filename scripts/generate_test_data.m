ee90f1ca45e9a15b8be1696cf3b65202bd6c3d66
Steve <stevenwasi@gmail.com>
2015-04-29 07:31:31 -0400

    added a script for generating the testing and training set

---
+++ scripts/generate_test_data.m
@@ -0,0 +1,35 @@
+function [ X, Y ] = generate_test_data( filename, percent )
+%GENERATE_TEST_DATA
+
+Data = dlmread(filename);
+
+[m n] = size(Data);
+
+%Make sure data is sorted by class
+Data = sortrows(Data);
+
+%Generate test and training dataset
+Test = [];
+Train = [];
+i = 0;
+while(i < m)
+    first = i + 1;
+    len = size(find(Data(:,1) == Data(first,1)),1);
+    i = i + len;
+    last = i;
+    
+    [trainInd,valInd,testInd] = dividerand(len, 1 - percent, 0, percent);
+    
+    Train = [Train; removerows(Data(first:last,:), testInd)];
+    Test = [Test; removerows(Data(first:last,:), trainInd)];
+end
+
+%Output files
+dlmwrite(strcat(filename, '.train'), Train, ' ');
+dlmwrite(strcat(filename, '.test'), Test, ' ');
+
+X = Train;
+Y = Test;
+
+end
+
