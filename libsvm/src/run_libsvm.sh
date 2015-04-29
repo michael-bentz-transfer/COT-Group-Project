fe52e4222a29e9eb3449db304e97ec24dccdd007
Steve <stevenwasi@gmail.com>
2015-04-29 09:50:20 -0400

    ran libsvm on all datasets

---
+++ libsvm/src/run_libsvm.sh
@@ -0,0 +1,4 @@
+python ../include/tools/easy.py ../datasets/haberman.train ../datasets/haberman.test;
+python ../include/tools/easy.py ../datasets/iris.train ../datasets/iris.test;
+python ../include/tools/easy.py ../datasets/survey.train ../datasets/survey.test;
+python ../include/tools/easy.py ../datasets/wine.train ../datasets/wine.test;
