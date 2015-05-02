0e2eddeb92e0b72174ed805a221117bd070b8825
Aaron Abbott <aabmass@gmail.com>
2015-05-02 17:55:33 -0400

    Display correct percentage predicted

--- scripts/clasifyiris.m
+++ scripts/clasifyiris.m
@@ -3,14 +3,26 @@ numToTrain = 50;
 
 [numInstances, numFeatures] = size(x);
 [~, numClasses] = size(y);
-yPredicted = zeros(numInstances - numToTrain, numClasses);
+numClassified = numInstances - numToTrain;
+yPredicted = zeros(numClassified, numClasses);
+fprintf('size(yPredicted\n');
+size(yPredicted)
 
-W
-x
+numCorrect = 0;
 
 % data is now already scaled
-for i = 1:numInstances
-    yPredicted(i,:) = (W' * x(i,:)')';
+% we trained on the first 1 though numToTrain, so start
+% predictions on numToTrain + 1 through numInstances
+for i = numToTrain + 1 : numInstances
+    predictionVector = (W' * x(i,:)')';
+    [maxCol, colIndex] = max(predictionVector);
+    yPredicted(i - numToTrain,colIndex) = 1;
+    
+    [~, actualIndex] = max(y(i,:));
+    if (colIndex == actualIndex)
+        numCorrect = numCorrect + 1;
+    end
 end
 
 yPredicted
+fprintf('Predicted %d correctly of %d -- %f %%\n', numCorrect, numClassified, numCorrect/numClassified);
